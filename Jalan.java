8a99383429c250ce313c0935706e9c6cf00db5da
jaki16 <154899188+jaki16@users.noreply.github.com>
2024-05-03 14:24:56 +0700

    Add files via upload

---
+++ Jalan.java
@@ -0,0 +1,55 @@
+import greenfoot.*;
+
+public class Jalan extends Actor
+{
+    private GreenfootImage[] frames; // Array untuk menyimpan frame dari sprite sheet
+    private int frameIndex; // Indeks frame saat ini
+    private int frameDelay = 5; // Jeda antara perubahan fraame
+    private int delayCount = 0; // Hitung jeda saat ini
+
+    public Jalan() {
+        // Memuat sprite sheet
+        GreenfootImage spriteSheet = new GreenfootImage("spritesheet (4).png");
+        
+        // Mendefinisikan jumlah frame dan inisialisasi array frames
+        int totalFrames = 5; // Jumlah total frame pada spritesheet
+        frames = new GreenfootImage[totalFrames];
+        
+        // Mengisi array frames dengan frame-frame dari sprite sheet
+        int frameWidth = spriteSheet.getWidth() / totalFrames; // Menghitung lebar setiap frame
+        int frameHeight = spriteSheet.getHeight(); // Tinggi frame sama dengan tinggi sprite sheet
+        
+        for (int i = 0; i < totalFrames; i++) {
+            frames[i] = new GreenfootImage(frameWidth, frameHeight);
+            frames[i].drawImage(spriteSheet, -i * frameWidth, 0);
+        }
+        
+        // Set frame awal
+        setImage(frames[0]);
+        frameIndex = 0; // Set frameIndex ke 0 saat awal
+    }
+
+    public void act()
+    {
+        // Pergerakan jalan
+        if (Greenfoot.isKeyDown("d")) {
+            setLocation(getX() + 5, getY()); // Gerak ke kanan
+        }
+        if (Greenfoot.isKeyDown("a")) {
+            setLocation(getX() - 5, getY()); // Gerak ke kiri
+        }
+        
+        // Mengubah frame setiap kali delayCount mencapai frameDelay
+        if (!Greenfoot.isKeyDown("a") && !Greenfoot.isKeyDown("d")) {
+            // Jika tidak ada tombol yang ditekan, tampilkan frame terakhir yang ditampilkan sebelumnya
+            setImage(frames[frameIndex]);
+        } else if (delayCount >= frameDelay) {
+            // Jika tombol "a" atau "d" ditekan, atur animasi berjalan
+            frameIndex = (frameIndex + 1) % frames.length;
+            setImage(frames[frameIndex]);
+            delayCount = 0; // Reset delayCount setelah mengubah frame
+        } else {
+            delayCount++; // Meningkatkan delayCount jika belum saatnya mengubah frame
+        }
+    }
+}
---
+++ bac.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class bac here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class bac extends World
+{
+
+    /**
+     * Constructor for objects of class bac.
+     * 
+     */
+    public bac()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(2000, 720, 1); 
+    }
+}
